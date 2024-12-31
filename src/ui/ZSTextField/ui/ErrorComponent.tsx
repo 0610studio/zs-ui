6bfebed8fc7dec87ffcbcf5bb6b7284051333e94
RightHot <39161206+KimJeonghun91@users.noreply.github.com>
2024-12-31 13:30:39 +0900

    SvgExclamation 추가 (#11)

--- src/ui/ZSTextField/ui/ErrorComponent.tsx
+++ src/ui/ZSTextField/ui/ErrorComponent.tsx
@@ -1,6 +1,6 @@
-import { View } from "react-native";
 import Animated, { FadeInDown } from "react-native-reanimated";
 import ZSText from "../../ZSText";
+import SvgExclamation from "../../../assets/SvgExclamation";
 
 const ErrorComponent = ({
   errorMessage,
@@ -11,13 +11,12 @@ const ErrorComponent = ({
 }) => {
   return (
     <Animated.View entering={FadeInDown} style={{ width: '100%', flexDirection: 'row', alignItems: 'center', paddingLeft: 5, marginTop: 6 }}>
-      <View style={{ width: 14, height: 14, justifyContent: 'center', alignItems: 'center', borderRadius: 30, backgroundColor: errorColor }}>
-        <ZSText allowFontScaling={false} typo='title.4' style={{ color: 'white', textAlign: 'center', textAlignVertical: 'center' }}>
-          {`!`}
-        </ZSText>
-      </View>
+      <SvgExclamation
+        size={16}
+        backgroundColor={errorColor}
+      />
 
-      <ZSText typo='body.4' style={{ marginLeft: 5, color: errorColor }}>
+      <ZSText typo='body.4' style={{ marginLeft: 4, color: errorColor }}>
         {errorMessage}
       </ZSText>
     </Animated.View>
