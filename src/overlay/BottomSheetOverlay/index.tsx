5669c75adcb3269e42358eb31144e215cc31f99c
RightHot <39161206+KimJeonghun91@users.noreply.github.com>
2025-04-03 22:16:01 +0900

    0.0.64 (#23)
    * BottomSheet 화면의 크기보다 높이가 높으면 화면의 크기로 제한
    
    * 버전 변경

--- src/overlay/BottomSheetOverlay/index.tsx
+++ src/overlay/BottomSheetOverlay/index.tsx
@@ -4,10 +4,10 @@ import { useBottomSheet } from '../../model/useOverlay';
 import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
 import ModalBackground from '../ui/ModalBackground';
 import { useTheme } from '../../model';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useSafeAreaInsets, initialWindowMetrics } from 'react-native-safe-area-context';
 import { ShowBottomSheetProps } from '../../model/types';
 
-const { width } = Dimensions.get('window');
+const { width, height: windowHeight } = Dimensions.get('window');
 
 function BottomSheetOverlay({
   headerComponent,
@@ -20,16 +20,17 @@ function BottomSheetOverlay({
     marginBottom = 10,
     padding = 14,
   } = options;
-
-  const [localVisible, setLocalVisible] = useState(false);
-  const { bottomSheetVisible, setBottomSheetVisible, height, setHeight } = useBottomSheet();
+  const { bottomSheetVisible, setBottomSheetVisible, height } = useBottomSheet();
+  // 화면의 크기보다 높이가 높으면 화면의 크기로 제한
+  const maxHeight = Math.min((windowHeight - 30 - (initialWindowMetrics?.insets.bottom || 0) - (initialWindowMetrics?.insets.top || 0)), height);
+  const translateY = useSharedValue(maxHeight);
   const translateX = useSharedValue(0);
-  const translateY = useSharedValue(height);
   const scale = useSharedValue(1);
   const { palette } = useTheme();
   const { bottom } = useSafeAreaInsets();
   const startX = useRef(0);
   const startY = useRef(0);
+  const [localVisible, setLocalVisible] = useState(false);
 
   // ** 소프트 키보드 핸들링
   useEffect(() => {
@@ -62,7 +63,7 @@ function BottomSheetOverlay({
         restDisplacementThreshold: 0.2,
       });
     } else {
-      translateY.value = withTiming(height + 100, { duration: 150 });
+      translateY.value = withTiming(maxHeight + 100, { duration: 150 });
       setTimeout(() => {
         setLocalVisible(false);
       }, 200);
@@ -105,8 +106,8 @@ function BottomSheetOverlay({
         translateX.value = withTiming(0, { duration: 100 });
 
         // 빠른 플리킹 제스처를 했을 때, 혹은 화면의 1/3 이상 내렸을 때, 닫기
-        if (gestureState.vy > 0.5 || translateY.value > height / 3) {
-          translateY.value = withTiming(height + 100, { duration: 150 });
+        if (gestureState.vy > 0.5 || translateY.value > maxHeight / 3) {
+          translateY.value = withTiming(maxHeight + 100, { duration: 150 });
           setBottomSheetVisible(false);
         } else {
           translateY.value = withTiming(0, { duration: 150 });
@@ -135,7 +136,7 @@ function BottomSheetOverlay({
           styles.container,
           {
             width: width - marginHorizontal * 2,
-            height,
+            height: maxHeight,
             marginHorizontal,
             bottom: marginBottom + bottom,
             backgroundColor: palette.background.base,
