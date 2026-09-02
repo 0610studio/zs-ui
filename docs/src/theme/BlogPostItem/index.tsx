2a8cfa851e9d5c6e51c367433b2ec581f2bd1f46
RightHot <39161206+KimJeonghun91@users.noreply.github.com>
2026-09-02 18:50:23 +0900

    1.0.1 (#106)

--- docs/src/theme/BlogPostItem/index.tsx
+++ docs/src/theme/BlogPostItem/index.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import BlogPostItem from '@theme-original/BlogPostItem';
 import type BlogPostItemType from '@theme/BlogPostItem';
 import type { WrapperProps } from '@docusaurus/types';
-import { useColorMode } from '@docusaurus/theme-common';
 import { useLocation } from '@docusaurus/router';
 import Giscus from '@giscus/react';
 
 type Props = WrapperProps<typeof BlogPostItemType>;
 
 export default function BlogPostItemWrapper(props: Props): JSX.Element {
-  const { colorMode } = useColorMode();
   const { pathname } = useLocation();
 
   return (
@@ -26,8 +24,7 @@ export default function BlogPostItemWrapper(props: Props): JSX.Element {
           reactionsEnabled="1"
           emitMetadata="1"
           inputPosition="bottom"
-          theme={colorMode === 'dark' ? 'dark' : 'light'}
-          data-theme="preferred_color_scheme"
+          theme="preferred_color_scheme"
           lang="ko"
           loading="lazy"
         />
